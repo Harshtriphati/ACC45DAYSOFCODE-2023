185f881261dfdec40aa79fdfef0020901737dfd4
Harsh_Tripathi <144989081+Harshtriphati@users.noreply.github.com>
2023-09-24 16:49:52 +0530

    Add files via upload

---
+++ Day 7 code.c
@@ -0,0 +1,11 @@
+// Code for Day 
+#include<stdio.h>
+
+int main() {
+// Code for printing the ASCII Value of any character 
+    int ch;
+    printf("Enter the character : ");
+    scanf("%c", &ch);
+    printf("ASCII Value of %c is %d", ch, ch);
+    return 0;
+}
