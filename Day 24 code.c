582352991e0dddd5da9969782c613aa4af26517c
Harsh_Tripathi <144989081+Harshtriphati@users.noreply.github.com>
2023-10-11 22:57:44 +0530

    Add files via upload

---
+++ Day 24 code.c
@@ -0,0 +1,13 @@
+// Code for Day 24
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+// Code to print Square of a number using library function
+    int n;
+    printf("Enter the number ; ");
+    scanf("%d", &n);
+    
+    printf("Square of the number is %f", pow(n, 2));
+    return 0;
+}
