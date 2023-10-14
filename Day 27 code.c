f68b80a40a8a308c7d0583b07a657145c1420f8b
Harsh_Tripathi <144989081+Harshtriphati@users.noreply.github.com>
2023-10-14 22:17:51 +0530

    Add files via upload

---
+++ Day 27 code.c
@@ -0,0 +1,11 @@
+// Code for Day 27
+#include<stdio.h>
+
+int main() {
+// Code to print alphabet using their ASCII value
+    int i;
+    for(i = 65; i <= 90; i++) {
+        printf("%c\n", i);
+    }
+    return 0;
+}
