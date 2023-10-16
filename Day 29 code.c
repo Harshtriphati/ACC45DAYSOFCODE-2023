2f9f2f6a92f44e1fe48c9c8f7a045bca8ed0b5a9
Harsh_Tripathi <144989081+Harshtriphati@users.noreply.github.com>
2023-10-16 19:24:37 +0530

    Add files via upload

---
+++ Day 29 code.c
@@ -0,0 +1,9 @@
+// Code For Day 29
+#include<stdio.h>
+
+int main() {
+// Code to use array to print a particular marks from a set of marks
+    int marks[15] = {86, 78, 90, 85, 59, 72, 97, 80, 57};
+    printf("%d\n", marks[5]);
+    return 0;
+}
