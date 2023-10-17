3d675ca4a47be22e15b0df9c8cbfae349c753456
Harsh_Tripathi <144989081+Harshtriphati@users.noreply.github.com>
2023-10-17 18:36:31 +0530

    Add files via upload

---
+++ Day 30 code.c
@@ -0,0 +1,13 @@
+// Code For Day 30
+#include<stdio.h>
+
+int main() {
+// Code to use loop inside array
+    int marks[] = {89, 74, 85, 80, 67, 81, 72, 83, 81, 86, 64, 79, 80};
+    int i;
+    
+    for(i =0; i < 13; i++){
+        printf("%d\n", marks[i]);
+    }
+    return 0;
+}
