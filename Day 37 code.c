3fc25cd946b4b4bcf9aa1948990eb4e0b77ee15a
Harsh_Tripathi <144989081+Harshtriphati@users.noreply.github.com>
2023-10-24 11:29:59 +0530

    Add files via upload

---
+++ Day 37 code.c
@@ -0,0 +1,19 @@
+// Code for Day 3000
+#include<stdio.h>
+
+int main() {
+// Code to print sum of all digits in a number entered by User 
+    int d, sum, num;
+    printf("Enter the number : ");
+    scanf("%d", &num);
+    sum = 0;
+    
+    while(num > 0) {
+        d = num % 10;
+        sum = sum + d;
+        num = num / 10;
+    }
+    printf("Sum of number of digits is the entered number is %d", sum);
+    
+    return 0;
+}
